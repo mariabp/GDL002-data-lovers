65d4a7d61612d8a0eacceebda5faec32be156e92
Maria Bolaños <mariadlp@gmail.com>
2019-02-20 08:24:11 -0600

    Agregué estructura HTML y CSS para poder iniciar

---
+++ src/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let userRequest = document.querySelector('#searchbar');
+const searchButton = document.querySelector('#searchbutton');
+const showAllButton = document.querySelector('#showall');
+const description = document.querySelector('h1');
+const footer = document.querySelector('footer');
+//const pokemonObj = POKEMON.pokemon;
+
+const showResults = () => {
+    searchButton.style.display = 'none';
+    userRequest.style.display = 'none';
+    showAllButton.style.display = 'none';
+    description.style.display = 'none';
+    footer.style.display = 'none';
+    }
+;
+
+
+searchButton.addEventListener('click', showResults);
