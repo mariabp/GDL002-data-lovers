a5377835d7a82378312648a05a2d2d19fcf7373d
Maria Bolaños <mariadlp@gmail.com>
2019-02-25 16:05:15 -0600

    Agregue las funciones vacias al objeto window.data segun indicaciones del readme.

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 'use strict';
-/*
-window.data = {
 
-};*/
+window.data = {
+    filterData: (data,condition) => {},
+    sortData: (data,sortBy,sortOrder) => {},
+    computeStats: (data) => {}
+};
