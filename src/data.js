37d03f1ae922f1557f7a45047a40fee86030cc45
Maria Bolaños <mariadlp@gmail.com>
2019-02-26 17:46:37 -0600

    Se agrego la funcion para filtrar por tipo.

--- src/data.js
+++ src/data.js
@@ -1,7 +1,17 @@
 'use strict';
 
-window.data = {
-    filterData: (data,condition) => {},
+window.kantodex = {
+    filterData: (data,condition) => {
+                    const filteredPokemonObj = data.filter(element => {
+                        for (let i = 0; i < element.type.length; i++) {
+                            if (element.type[i] == condition) {
+                                return element;
+                            }
+                        }
+                    });
+                    return filteredPokemonObj;
+
+    },
     sortData: (data,sortBy,sortOrder) => {},
     computeStats: (data) => {}
 };
