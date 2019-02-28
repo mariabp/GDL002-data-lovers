1dd47d9b80209dc7538d76520b68d4d2aa289941
Maria Bolaños <mariadlp@gmail.com>
2019-02-28 12:45:42 -0600

    Funcion SortData funcionando en objeto kandotex.

--- src/data.js
+++ src/data.js
@@ -4,42 +4,26 @@ window.kantodex = {
 //Funcion para filtrar por tipo:
     filterData: (data,setCondition) => {
         data;
-        //console.log(data);
         const filteredPokemonObj = data.filter(element => {
             for (let i = 0; i < element.type.length; i++) {
                 if (element.type[i] == setCondition) {
                     return element;
                 }
-               // console.log(element);
             }
         });
         return filteredPokemonObj;
-    }
-
-};
-/*    
-
-filterData: (pokemonObj, pokemonList, setCondition) => {
-
-        console.log(pokemonObj);
-
-        if (pokemonList === []) {
-            pokemonList = pokemonObj;
-        }
-
-        let filteredPokemonList = [];
-
-        console.log(filteredPokemonList);
-
-        pokemonList.forEach((element) => {
-
-            element['type'].forEach((element) => {
-                if (element === `${setCondition}`) {
-                    filteredPokemonList.push(element);
-                    console.log(filteredPokemonList);
-                }
-            });
+    },
+    sortData:(data) => {
+        const nameList = data.map(element=> {
+            return element.name;
         });
-    }
 
-    }*/ 
+        const orderedNames = nameList.sort(function(a,b){
+            return a.localeCompare(b);
+        });
+
+        const orderedObjects = orderedNames.map(name => data.find(element => element.name === name));
+        return orderedObjects;
+    }
+};
+ 
