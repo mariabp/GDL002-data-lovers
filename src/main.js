1a383aeb150f859eb1802cecf69dc32e7e76204a
Maria Bolaños <mariadlp@gmail.com>
2019-03-10 16:49:52 -0600

    Funciones abstractas y funcionando. Clic en pokemon y despliega la info

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ const validateSearchInput = () => {
 
 };
 
-/*Funcion que cambia de color el campo donde el usuario ingresa su busqueda,resetea valor de busqueda del usuario a vacio y esconde el contenedor de resultados */
+//Funcion que cambia de color el campo donde el usuario ingresa su busqueda,resetea valor de busqueda del usuario a vacio y esconde el contenedor de resultados */
 const warnAndReset = () => {
 
 	searchInput.style.backgroundColor =  "rgb(212, 110, 110)";
@@ -97,42 +97,83 @@ const colorBgAndShow = () => {
 
 };
 
-//Funcion que obtiene la información de 1 pokemon buscado.
-const searchPokemon = () => {
+const pokemonInfoLayout = (givenPokemonList) => {
 
-	let searchedPokemon = searchInput.value.toLowerCase();
-	pokemonObj.forEach((element) => {
+	results.innerHTML = "";
+
+	let secondaryInfoId = 1;
+
+	const showSecondaryInfo = () => {
+
+		let pokemonInfoDiv = event.currentTarget;
+		let clickedElement = pokemonInfoDiv.querySelector("[id^='secondaryinfoid']");
+		clickedElement.style.display = "grid";
+
+	};
+	
+	givenPokemonList.forEach((element) => {
+
 		pokemonInfo = `
-			<div class="pokemoninfo">
-			<div id="pokemonid">${element.id}</div>
-			<div id="pokemonname">${element.name}</div>
-			<img id="pokemonimg" src="${element.img}">
-			<div id="pokemontype"><div class="property">Type: </div>${element.type}</div>
-			<div id="pokemonheight"><div class="property">Height: </div>${element.height}</div>
-			<div id="pokemonweight"><div class="property">Weight: </div>${element.weight}</div>
-			<div id="pokemoncandy"><div class="property">Candy: </div>${element.candy}</div>
-			<div id="pokemoncandycount"><div class="property">Candy Count: </div>${element.candy_count}</div>
-			<div id="pokemonegg"><div class="property">Egg: </div>${element.egg}</div>
-			<div id="pokemonspawnchance"><div class="property">Spawn Chance: </div>${element.spawn_chance}</div>
-			<div id="pokemonavgspawns"><div class="property">Average Spawns: </div>${element.avg_spawns}</div>
-			<div id="pokemonspawntime"><div class="property">Spawn Time: </div>${element.spawn_time}</div>
-			<div id="pokemonmultipliers"><div class="property">Multipliers: </div>${element.multipliers}</div>
-			<div id="pokemonweaknesses"><div class="property">Weaknesses: </div>${element.weaknesses}</div>
-			<div id="pokemonprevevolution"><div class="property">Previous Evolution: </div>${element.next_evolution}</div>
-			<div id="pokemonnextevolution"><div class="property">Next Evolution: </div>${element.next_evolution}</div>
+			<div id="identifier${element.id}" class="pokemoninfo">
+
+				<div class="primaryinfolayout">
+					<div id="pokemonid">${element.id}</div>
+					<div id="pokemonname">${element.name}</div>
+					<img id="pokemonimg" src="${element.img}">
+				</div>
+
+				<div id="secondaryinfoid${secondaryInfoId}" class="secondaryinfolayout">
+					<div id="pokemontype" class="secondaryinfostyle"><div class="property">Type: </div>${element.type}</div>
+					<div id="pokemonheight" class="secondaryinfostyle"><div class="property">Height: </div>${element.height}</div>
+					<div id="pokemonweight" class="secondaryinfostyle"><div class="property">Weight: </div>${element.weight}</div>
+					<div id="pokemoncandy" class="secondaryinfostyle"><div class="property">Candy: </div>${element.candy}</div>
+					<div id="pokemoncandycount" class="secondaryinfostyle"><div class="property">Candy Count: </div>${element.candy_count}</div>
+					<div id="pokemonegg" class="secondaryinfostyle"><div class="property">Egg: </div>${element.egg}</div>
+					<div id="pokemonspawnchance" class="secondaryinfostyle"><div class="property">Spawn Chance: </div>${element.spawn_chance}</div>
+					<div id="pokemonavgspawns" class="secondaryinfostyle"><div class="property">Average Spawns: </div>${element.avg_spawns}</div>
+					<div id="pokemonspawntime" class="secondaryinfostyle"><div class="property">Spawn Time: </div>${element.spawn_time}</div>
+					<div id="pokemonmultipliers" class="secondaryinfostyle"><div class="property">Multipliers: </div>${element.multipliers}</div>
+					<div id="pokemonweaknesses" class="secondaryinfostyle"><div class="property">Weaknesses: </div>${element.weaknesses}</div>
+					<div id="pokemonprevevolution" class="secondaryinfostyle"><div class="property">Previous Evolution: </div>${element.next_evolution}</div>
+					<div id="pokemonnextevolution" class="secondaryinfostyle"><div class="property">Next Evolution: </div>${element.next_evolution}</div>
+				</div>
+				
 			</div>`
 		;
 
+		secondaryInfoId++;
+		results.innerHTML += pokemonInfo;
+		
+	});
+
+
+	let pokemonInfoDisplayIds = document.querySelectorAll("[id^='identifier']");
+
+	pokemonInfoDisplayIds.forEach((element) => {
+		element.addEventListener('click', showSecondaryInfo);
+	});
+
+	colorBgAndShow();
+	
+};
+
+//Funcion que obtiene la información de 1 pokemon buscado.
+const searchPokemon = () => {
+
+	pokemonList = [];
+
+	let searchedPokemon = searchInput.value.toLowerCase();
+
+	pokemonObj.forEach((element) => {
+
 		if (element.name.toLowerCase().includes(`${searchedPokemon}`) || element.id === parseInt(searchedPokemon)) {
 			pokemonList.push(element);
-			results.innerHTML += pokemonInfo;
-		} else {
-			searchInput.value = '';
-			searchInput.placeholder = "Tu búsqueda no arrojó resultados";
-			searchInput.style.backgroundColor =  "rgb(196, 187, 107)";
 		}
+
 	});
-	colorBgAndShow();
+
+	pokemonInfoLayout(pokemonList);
+
 };
 
 //Funcion que obtiene e imprime una lista todos los pokemon
@@ -140,151 +181,39 @@ const getAllPokemon = () => {
 
 	pokemonList = [];
 
-	results.innerHTML = "";
-	
 	pokemonObj.forEach((element) => {
-
-		pokemonInfo = `
-			<div class="pokemoninfo">
-			<div id="pokemonid">${element.id}</div>
-			<div id="pokemonname">${element.name}</div>
-			<img id="pokemonimg" src="${element.img}">
-			<div id="pokemontype"><div class="property">Type: </div>${element.type}</div>
-			<div id="pokemonheight"><div class="property">Height: </div>${element.height}</div>
-			<div id="pokemonweight"><div class="property">Weight: </div>${element.weight}</div>
-			<div id="pokemoncandy"><div class="property">Candy: </div>${element.candy}</div>
-			<div id="pokemoncandycount"><div class="property">Candy Count: </div>${element.candy_count}</div>
-			<div id="pokemonegg"><div class="property">Egg: </div>${element.egg}</div>
-			<div id="pokemonspawnchance"><div class="property">Spawn Chance: </div>${element.spawn_chance}</div>
-			<div id="pokemonavgspawns"><div class="property">Average Spawns: </div>${element.avg_spawns}</div>
-			<div id="pokemonspawntime"><div class="property">Spawn Time: </div>${element.spawn_time}</div>
-			<div id="pokemonmultipliers"><div class="property">Multipliers: </div>${element.multipliers}</div>
-			<div id="pokemonweaknesses"><div class="property">Weaknesses: </div>${element.weaknesses}</div>
-			<div id="pokemonprevevolution"><div class="property">Previous Evolution: </div>${element.next_evolution}</div>
-			<div id="pokemonnextevolution"><div class="property">Next Evolution: </div>${element.next_evolution}</div>
-			</div>`
-		;
-
 		pokemonList.push(element);
-		results.innerHTML += pokemonInfo;
-
 	});
 
-	colorBgAndShow();
+	pokemonInfoLayout(pokemonList);
 	
 };
 
 //Funcion para imprimir la lista por orden alfabético
 const printSortedList = () => {
-
-	results.innerHTML = "";
-
-	kantodex.sortData(pokemonList).forEach((element) => {
-
-		pokemonInfo = `
-			<div class="pokemoninfo">
-			<div id="pokemonid">${element.id}</div>
-			<div id="pokemonname">${element.name}</div>
-			<img id="pokemonimg" src="${element.img}">
-			<div id="pokemontype"><div class="property">Type: </div>${element.type}</div>
-			<div id="pokemonheight"><div class="property">Height: </div>${element.height}</div>
-			<div id="pokemonweight"><div class="property">Weight: </div>${element.weight}</div>
-			<div id="pokemoncandy"><div class="property">Candy: </div>${element.candy}</div>
-			<div id="pokemoncandycount"><div class="property">Candy Count: </div>${element.candy_count}</div>
-			<div id="pokemonegg"><div class="property">Egg: </div>${element.egg}</div>
-			<div id="pokemonspawnchance"><div class="property">Spawn Chance: </div>${element.spawn_chance}</div>
-			<div id="pokemonavgspawns"><div class="property">Average Spawns: </div>${element.avg_spawns}</div>
-			<div id="pokemonspawntime"><div class="property">Spawn Time: </div>${element.spawn_time}</div>
-			<div id="pokemonmultipliers"><div class="property">Multipliers: </div>${element.multipliers}</div>
-			<div id="pokemonweaknesses"><div class="property">Weaknesses: </div>${element.weaknesses}</div>
-			<div id="pokemonprevevolution"><div class="property">Previous Evolution: </div>${element.next_evolution}</div>
-			<div id="pokemonnextevolution"><div class="property">Next Evolution: </div>${element.next_evolution}</div>
-			</div>`
-			;
-
-
-		results.innerHTML += pokemonInfo;
-	}
-	);
-
-	colorBgAndShow();
+	
+	pokemonInfoLayout(kantodex.sortData(pokemonList));
 
 };
 
 const printSortedDescentList = () => {
 
-	results.innerHTML = "";
-
-	kantodex.sortDataDescent(pokemonList).forEach((element) => {
-
-		pokemonInfo = `
-			<div class="pokemoninfo">
-			<div id="pokemonid">${element.id}</div>
-			<div id="pokemonname">${element.name}</div>
-			<img id="pokemonimg" src="${element.img}">
-			<div id="pokemontype"><div class="property">Type: </div>${element.type}</div>
-			<div id="pokemonheight"><div class="property">Height: </div>${element.height}</div>
-			<div id="pokemonweight"><div class="property">Weight: </div>${element.weight}</div>
-			<div id="pokemoncandy"><div class="property">Candy: </div>${element.candy}</div>
-			<div id="pokemoncandycount"><div class="property">Candy Count: </div>${element.candy_count}</div>
-			<div id="pokemonegg"><div class="property">Egg: </div>${element.egg}</div>
-			<div id="pokemonspawnchance"><div class="property">Spawn Chance: </div>${element.spawn_chance}</div>
-			<div id="pokemonavgspawns"><div class="property">Average Spawns: </div>${element.avg_spawns}</div>
-			<div id="pokemonspawntime"><div class="property">Spawn Time: </div>${element.spawn_time}</div>
-			<div id="pokemonmultipliers"><div class="property">Multipliers: </div>${element.multipliers}</div>
-			<div id="pokemonweaknesses"><div class="property">Weaknesses: </div>${element.weaknesses}</div>
-			<div id="pokemonprevevolution"><div class="property">Previous Evolution: </div>${element.next_evolution}</div>
-			<div id="pokemonnextevolution"><div class="property">Next Evolution: </div>${element.next_evolution}</div>
-			</div>`
-			;
-
-
-		results.innerHTML += pokemonInfo;
-	}
-	);
-
-	colorBgAndShow();
+	pokemonInfoLayout(kantodex.sortDataDescent(pokemonList));		
 
 };
 
 //Funcion para imprimir lista de Pokemon filtrados
 const printFilteredPokemon = () => {
 
-		results.innerHTML = "";
+	if (pokemonList.length === 0) {
+		pokemonList = pokemonObj;
+	}
 
-		if (pokemonList.length === 0) {
-			pokemonList = pokemonObj;
-		}
+	pokemonList = kantodex.filterData(pokemonList, condition.value);
 
-        kantodex.filterData(pokemonList, condition.value).forEach((element) => {
-            let pokemonInfo = 
-                `
-                <div class="pokemoninfo">
-                <div id="pokemonid">${element.id}</div>
-                <div id="pokemonname">${element.name}</div>
-                <img id="pokemonimg" src="${element.img}">
-                <div id="pokemontype">Type: ${element.type}</div>
-                <div id="pokemonheight">Height: ${element.height}</div>
-                <div id="pokemonweight">Weight: ${element.weight}</div>
-                <div id="pokemoncandy">Candy: ${element.candy}</div>
-                <div id="pokemoncandycount">Candy Count: ${element.candy_count}</div>
-                <div id="pokemonegg">Egg: ${element.egg}</div>
-                <div id="pokemonspawnchance">Spawn Chance: ${element.spawn_chance}</div>
-                <div id="pokemonavgspawns">Average Spawns: ${element.avg_spawns}</div>
-                <div id="pokemonspawntime">Spawn Time: ${element.spawn_time}</div>
-                <div id="pokemonmultipliers">Multipliers: ${element.multipliers}</div>
-                <div id="pokemonweaknesses">Weaknesses: ${element.weaknesses}</div>
-                <div id="pokemonnextevolution">Next Evolution: ${element.next_evolution}</div>
-                </div>
-                `
-			;
-			results.innerHTML += pokemonInfo;
-		});
+	pokemonInfoLayout(pokemonList);
 
-		pokemonList = kantodex.filterData(pokemonList, condition.value);
-		colorBgAndShow();
-
-	};
+};
 
 //Funcion para mostrar About Kanto Dex
 const showAboutUs = () => {
@@ -343,4 +272,3 @@ sortDescentButton.addEventListener('click', printSortedDescentList);
 homeButton.addEventListener('click', home);
 infoButton.addEventListener('click', showStatistics);
 calculateButton.addEventListener('click', getCalculations);
-
